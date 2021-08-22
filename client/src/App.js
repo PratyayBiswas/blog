7ee7388366ee6ab30298a290f4ae5f6255251638
PratyayBiswas <pratyaybiswas.8@gmail.com>
2021-08-22 21:55:14 +0530

    header ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Header from './components/Header';
 
 const App = () => {
-    return(
-        <div>
-            <Header />
+    return (
+        <div className="container-main">
+            <div className="container">
+                <Header />
+            </div>
+
         </div>
     );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,30 @@ const Header = () => {
     return (
         <header>
             <div className="header-top">
-                <h1 className="tilte-page">
+                <h1 className="zero">
                     <a href="">Title</a>
                 </h1>
                 <form action="">
                     <input type="text" placeholder="Search" />
+                    <button>Search</button>
                 </form>
             </div>
-
             <span>Stories of service from around the world</span>
+            <img src="/assets/images/stars.jpg" alt="not" />
+            <nav className="navbar">
+                <ul className="navbar-ul">
+                    <li className="navbar-li"><a className="navbar-a" href="#">Welcome</a></li>
+                    <li className="navbar-li"><a className="navbar-a" href="#">Stories</a></li>
+                    <li className="navbar-li"><a className="navbar-a" href="#">Contributor guidelines</a></li>
+                    <li className="navbar-li" ><a className="navbar-a topic-tab" href="">Topics</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
